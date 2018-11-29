8616ed493184e357117d025fe5bc76d19d0b5090
Connor <cgallim3@uncc.edu>
2018-11-29 23:10:33 +0000

    Install and set up cucumber

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1076146960366-mgbelpqg36pd5h30gf8ltuguqlj3qs0f.apps.googleusercontent.com', 'zqw8WKqwXBOB7QeVtN92QfDM', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
