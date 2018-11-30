946a30539033fc386ffd564e71afee525d6d2dd3
Connor <cgallim3@uncc.edu>
2018-11-30 00:46:10 +0000

    ability to write test without ouath messing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :contacts 
-  
   resources :users do
     resources :contacts
   end
--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,8 @@ ActiveRecord::Schema.define(version: 2018_11_09_191551) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "provider"
     t.string "uid"
     t.string "name"
-    t.string "oauth_token"
-    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
